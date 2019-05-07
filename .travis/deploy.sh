88b93433c51f457b937d50b6acb4c696652945a3
Vincent Stange <vinc.sohn@gmail.com>
2019-05-07 14:38:48 +0200

    Still not getting through with the git push

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,14 +4,11 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 ~/.ssh/travis-git # Allow read access to the private key
 ssh-add ~/.ssh/travis-git # Add the private key to SSH
 
-echo here1
-
 git config --global push.default matching
-echo here1
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
-echo here2
+echo here1
 git push deploy master
-echo here3
+echo here2
 
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh apps@$IP -p $PORT <<EOF
