c4f06389031e2eb9628210cdd00941f3a4b9a41c
Vincent Stange <vinc.sohn@gmail.com>
2019-05-07 10:32:02 +0200

    Added chmod x for deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -13,7 +13,7 @@ git push deploy master
 
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh apps@$IP -p $PORT <<EOF
-#  cd $DEPLOY_DIR
-#  crystal build --release --no-debug index.cr # Change to whatever commands you need!
   touch was_here.txt
 EOF
+#  cd $DEPLOY_DIR
+#  crystal build --release --no-debug index.cr # Change to whatever commands you need!
