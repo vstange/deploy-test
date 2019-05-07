6736bcb9b40b2ce2b5c443d141b7e459fe7b2a9f
Vincent Stange <vinc.sohn@gmail.com>
2019-05-07 14:59:52 +0200

    Finally ssh known host working, back to git

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,12 +6,10 @@ ssh-add ~/.ssh/travis-git # Add the private key to SSH
 
 git config --global push.default matching
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
-echo here1
 git push deploy master
-echo here2
 
 # Skip this command if you don't need to execute any additional commands after deploying.
-ssh apps@$IP -p $PORT <<EOF
+ssh git@$IP -p $PORT <<EOF
   touch was_here.txt
 EOF
 #  cd $DEPLOY_DIR
